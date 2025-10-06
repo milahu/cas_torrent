27a73740966930765cc05f52e9f4dff0758228e2
Milan Hauth <milahu@gmail.com>
2025-10-06 13:00:33 +0200

    up shell.nix

--- shell.nix
+++ shell.nix
@@ -1,16 +1,25 @@
 { pkgs ? import <nixpkgs> { } }:
 
-let
+let pp2 = {
   rtorrent-rpc = pkgs.python3.pkgs.callPackage ./nix/rtorrent-rpc {
     bencode2 = pkgs.python3.pkgs.callPackage ./nix/bencode2 { };
   };
-in
+  torrent-models = pkgs.python3.pkgs.callPackage ./nix/torrent-models {
+    bencode-rs = pkgs.python3.pkgs.callPackage ./nix/bencode-rs { };
+  };
+  tree-sitter = pkgs.python3.pkgs.callPackage ./nix/tree-sitter { };
+}; in
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
-    (python3.withPackages (pp: with pp; [
+    (python3.withPackages (pp: (with pp; [
       qbittorrent-api
+    ]) ++ (with pp2; [
       rtorrent-rpc
-    ]))
+      # torrent-models
+      tree-sitter
+    ])))
+    # pp2.tree-sitter
+    tree-sitter
   ];
 }
