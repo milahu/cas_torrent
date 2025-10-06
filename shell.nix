e9be5e9018bab480c9a628fa477c7b2794d50f10
Milan Hauth <milahu@gmail.com>
2025-10-06 15:07:54 +0200

    Revert "up shell.nix"
    This reverts commit 27a73740966930765cc05f52e9f4dff0758228e2.

--- shell.nix
+++ shell.nix
@@ -1,25 +1,16 @@
 { pkgs ? import <nixpkgs> { } }:
 
-let pp2 = {
+let
   rtorrent-rpc = pkgs.python3.pkgs.callPackage ./nix/rtorrent-rpc {
     bencode2 = pkgs.python3.pkgs.callPackage ./nix/bencode2 { };
   };
-  torrent-models = pkgs.python3.pkgs.callPackage ./nix/torrent-models {
-    bencode-rs = pkgs.python3.pkgs.callPackage ./nix/bencode-rs { };
-  };
-  tree-sitter = pkgs.python3.pkgs.callPackage ./nix/tree-sitter { };
-}; in
+in
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
-    (python3.withPackages (pp: (with pp; [
+    (python3.withPackages (pp: with pp; [
       qbittorrent-api
-    ]) ++ (with pp2; [
       rtorrent-rpc
-      # torrent-models
-      tree-sitter
-    ])))
-    # pp2.tree-sitter
-    tree-sitter
+    ]))
   ];
 }
